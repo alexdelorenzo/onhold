25fabb3bce14304f93703f68bbb11986b0a53206
Alex DeLorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2020-09-26 01:35:27 -0400

    Update after.py

--- onhold/after.py
+++ onhold/after.py
@@ -23,10 +23,10 @@ def run(file: Path):
 @click.command(help="Play specified sound after job is complete.")
 @click.option('-s', '--sound_path', required=False,
   type=click.Path(exists=True), help="Path to sound to play.")
-@click.option('-i', '--ignore', required=False,
-  is_flag=True, default=False, help="Suppress warnings.")
-def cmd(sound_path: Optional[str], ignore: bool):
-  with using_path(sound_path, ignore, DEFAULT_SOUND, ENV_VAR) as path:
+@click.option('-w', '--warn', required=False,
+  is_flag=True, default=False, help="Show warnings.")
+def cmd(sound_path: Optional[str], warn: bool):
+  with using_path(sound_path, warn, DEFAULT_SOUND, ENV_VAR) as path:
     run(path)
 
 
