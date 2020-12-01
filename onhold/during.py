c60b9b04ff8e0b4ab0a2d3a910b8defcd77b8008
Alex DeLorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2020-12-01 16:03:52 -0500

    Add terminal bell option to onhold

--- onhold/during.py
+++ onhold/during.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from play_sounds import play_while_running
 import click
 
-from .base import dumb_pipe, using_path, is_pipeline
+from .base import dumb_pipe, using_path, is_pipeline, bell as play_bell
 
 
 def run(file: Optional[Path] = None):
@@ -19,11 +19,16 @@ def run(file: Optional[Path] = None):
 @click.command(help="""Play the specified sound file while data is passed in through standard input and passed through standard output.""")
 @click.option('-s', '--sound_path', required=False,
   type=click.Path(exists=True), help="Path to sound to play.")
+@click.option('-b', '--bell', required=False, default=False, show_default=True,
+  type=click.Path(exists=True), help="Ring terminal bell, as well.")
 @click.option('-w', '--warn', required=False,
   is_flag=True, default=False, help="Show warnings.")
-def cmd(sound_path: Optional[str], warn: bool):
+def cmd(sound_path: Optional[str], bell: bool, warn: bool):
   with using_path(sound_path, warn) as path:
     run(path)
+
+  if bell:
+    play_bell()
 
 
 if __name__ == "__main__":
