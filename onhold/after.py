d41424ab5efaea581ed2645d808abb691c313e94
Alex DeLorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2020-12-06 16:23:51 -0500

    Clear indentation

--- onhold/after.py
+++ onhold/after.py
@@ -28,7 +28,7 @@ def run(file: Optional[Path]):
   is_flag=True, default=False, help="Show warnings.")
 def cmd(sound_path: Optional[str], bell: bool, warn: bool):
   with bell_after(bell), \
-    using_path(sound_path, warn, DEFAULT_SOUND, ENV_VAR) as path:
+       using_path(sound_path, warn, DEFAULT_SOUND, ENV_VAR) as path:
     run(path)
 
 
